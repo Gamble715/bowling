8bd3c0428e129e16c64eb21a887c7ec88b93e21b
Daniel Gamble McAdam <daniel.gamble.mcadam@gmail.com>
2013-09-12 14:48:39 -0500

    Refactor to make score method less... smelly.

--- bowling_game.rb
+++ bowling_game.rb
@@ -2,6 +2,8 @@ class BowlingGame
 	attr_accessor :rolls
 	def initialize
 		@rolls = []
+    @total_score = 0
+    @current_roll = 0
 	end
 	# Record a roll in the game.
 	#
@@ -14,25 +16,62 @@ class BowlingGame
 		# Returns the Integer score for this game. Expects to be run after all rolls
 		# for the game have been recorded.
 	def score
-  total_score  = 0
-  current_roll = 0
+  while @current_roll < @rolls.size - 1
+    init_roll
 
-  while current_roll < @rolls.size - 1
-    roll      = @rolls[current_roll]
-    next_roll = @rolls[current_roll + 1]
-
-    if roll == 10
-    	total_score += 10 + next_roll + @rolls[current_roll + 2]
-    	current_roll +=1
-    elsif roll + next_roll == 10
-      total_score += 10 + @rolls[current_roll + 2]
-      current_roll += 2
-    else
-      total_score += roll + next_roll
-      current_roll += 2
+    if strike?
+    	score_strike
+    elsif spare?
+      score_spare
+    else 
+      score_norm
     end
   end
 
-  	return total_score
+  	return @total_score
 	end
+
+  private
+
+  # Initialize values for roll and next_roll.
+  #
+  # Returns nothing.
+  def init_roll
+    @roll      = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll + 1]
+  end
+
+  # Returns true if current roll is strike, false if not.
+  def strike?
+    @roll == 10
+  end
+
+  # Scores a strike frame and adds it to the total score for the game.
+  #
+  # Returns nothing.
+  def score_strike
+    @total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+    @current_roll +=1
+  end
+
+  # Returns true if current roll is spare, false if not.
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+  # Scores a strike frame and adds it to the total score for the game.
+  #
+  # Returns nothing.
+  def score_spare
+    @total_score += 10 + @rolls[@current_roll + 2]
+    @current_roll += 2
+  end
+
+  # Adds both frames to score a normal non-spare and non-strike frame.
+  #
+  # Returns nothing.
+  def score_norm
+    @total_score += @roll + @next_roll
+    @current_roll += 2
+  end
 end
